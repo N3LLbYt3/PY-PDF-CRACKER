da2e77e48638701c8e9983c2ac5883b0933b3efb
𝓝𝓾𝓵𝓵𝓑𝔂𝓽𝓮 <129080315+N3LLbYt3@users.noreply.github.com>
2025-03-08 20:39:34 +0330

    Update Showing.py

--- core/Showing.py
+++ core/Showing.py
@@ -2,16 +2,16 @@ from colorama import Fore
 
 
 def print_error(message):
-    print(Fore.LIGHTRED_EX + "[-]" + message + Fore.RESET)
+    print(Fore.LIGHTRED_EX + "[-]" + Fore.RESET, message)
 
 
 def print_success(message):
-    print(Fore.LIGHTGREEN_EX + "[+]" + message + Fore.RESET)
+    print(Fore.LIGHTGREEN_EX + "[+]" + Fore.RESET, message)
 
 
 def print_info(message):
-    print(Fore.LIGHTMAGENTA_EX + "[i]" + message + Fore.RESET)
+    print(Fore.LIGHTMAGENTA_EX + "[i]" + Fore.RESET, message)
 
 
 def print_warning(message):
-    print(Fore.LIGHTYELLOW_EX + "[!]" + message + Fore.RESET)
+    print(Fore.LIGHTYELLOW_EX + "[!]" + Fore.RESET, message)
